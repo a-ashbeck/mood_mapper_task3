e8c8efb38cae6e1afb8b8d70792db81f098eac95
a-ashbeck <awashbeck@gmail.com>
2017-08-03 09:27:25 -0500

    Remove CSS

--- controllers/controller.js
+++ controllers/controller.js
@@ -17,7 +17,18 @@ module.exports = function(app) {
             q1: survey.q1,
             q2: survey.q2,
             q3: survey.q3,
-            q4: survey.q4
+            q4: survey.q4,
+            q5: survey.q5,
+            q6: survey.q6,
+            q7: survey.q7,
+            q8: survey.q8,
+            q9: survey.q9,
+            q10: survey.q10,
+            q11: survey.q11,
+            q12: survey.q12,
+            q13: survey.q13,
+            q14: survey.q14,
+            q15: survey.q15
         }).then(function() {
             response.redirect('/');
         }).catch(function(err) {
